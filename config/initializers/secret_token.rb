e9c0f27c5efcc86e89a6710a0aaf505ab1e65ab2
abhisekpanigrahi <abhisekpanigrahi@gmail.com>
2013-06-26 19:57:00 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Santa::Application.config.secret_token = '5310c2586a1a599147c6f1a5e48e45af066a233e455825ed7e195d5d961bd9494778e3a911df69c76b3957d2898d09e186e07e281d84ed779849e3ad06ed5fa4'
